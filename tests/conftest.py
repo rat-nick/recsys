f87563ac3c44b856e203ed65e8815862c6ccb21e
Nikola Ratinac <nikola.ratinac@gmail.com>
2023-07-12 22:54:52 +0200

    setup conf for testing with pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,40 @@
+import pandas as pd
 import pytest
-from core.dataset import Dataset
 
 
 @pytest.fixture
-def dataset():
-    pass
+def ratings_df():
+    df = pd.read_csv(
+        "data/ml-1m/ratings.csv",
+        sep=",",
+        engine="python",
+        encoding="latin-1",
+        low_memory=True,
+    )
+
+    return df
+
+
+@pytest.fixture
+def items_df():
+    df = pd.read_csv(
+        "data/ml-1m/movies.csv",
+        sep="::",
+        engine="python",
+        encoding="latin-1",
+        low_memory=True,
+    )
+
+    return df
+
+
+def users_df():
+    df = pd.read_csv(
+        "data/ml-1m/users.csv",
+        sep=",",
+        engine="python",
+        encoding="latin-1",
+        low_memory=True,
+    )
+
+    return df
