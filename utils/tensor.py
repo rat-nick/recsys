e63bd7a00fab02beb40ea59822e2b0a97c1b1613
ratinac-nikola <ratinac.nikola@gmail.com>
2023-07-14 19:51:39 +0200

    reimplement split to work for n-dim tensors

--- utils/tensor.py
+++ utils/tensor.py
@@ -7,18 +7,18 @@ from sklearn.model_selection import train_test_split
 def split(
     t: torch.Tensor, ratio: float = 0.2, nonzero_only: bool = True
 ) -> Tuple[torch.Tensor, torch.Tensor]:
-    # all_but_first = lambda x: tuple([t for t in range(1, len(x.shape))])
-    # reduce_to_first = lambda x: x.sum(dim=all_but_first(x))
-
     idx = t.nonzero()
     train_idx, test_idx = train_test_split(
         idx, test_size=ratio, random_state=42, shuffle=True
     )
-    train = torch.zeros_like(t)
-    idx = train_idx
-    train[idx[:, 0], idx[:, 1]] = t[idx[:, 0], idx[:, 1]]
 
+    train = torch.zeros_like(t)
     test = torch.zeros_like(t)
-    idx = test_idx
-    test[idx[:, 0], idx[:, 1]] = t[idx[:, 0], idx[:, 1]]
+
+    # Set nonzero values in train tensor
+    train[tuple(train_idx.unbind(1))] = t[tuple(train_idx.unbind(1))]
+
+    # Set nonzero values in test tensor
+    test[tuple(test_idx.unbind(1))] = t[tuple(test_idx.unbind(1))]
+
     return train, test
