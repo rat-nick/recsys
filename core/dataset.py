57215da2d2dad55099234e886e6721efc74024a7
Nikola Ratinac <ratinac.nikola@gmail.com>
2023-07-14 15:08:49 +0200

    implement Dataset and DatasetFactory

--- core/dataset.py
+++ core/dataset.py
@@ -1,27 +1,75 @@
+import logging
 from typing import Callable, Dict, Tuple, Union
 
 import pandas as pd
 import surprise
 from sklearn.model_selection import train_test_split
 
-from utils.data import build_full_df, str_to_timestamp
+from utils.data import build_full_df
 
 
 class Dataset:
+    def __init__(
+        self, trainset: surprise.Trainset, mode: Union["user", "item"] = "user"
+    ):
+        self.trainset = trainset
+        self.mode = mode
+
+    def user_ratings(self) -> Dict:
+        return self.trainset.ur
+
+    def item_ratings(self) -> Dict:
+        return self.trainset.ir
+
+    @property
+    def n_features(self) -> int:
+        return self.n_items if self.mode == "user" else self.n_users
+
+    @property
+    def n_items(self) -> int:
+        return self.trainset.n_items
+
+    @property
+    def n_users(self) -> int:
+        return self.trainset.n_users
+
+    @property
+    def n_ratings(self) -> int:
+        return self.trainset.n_ratings
+
+    def tvt(self, mode: Union["item", "user"] = "item") -> Tuple[Dict, Dict, Dict]:
+        if mode == "user":
+            ratings = self.user_ratings()
+            n_features = self.n_items
+        elif mode == "item":
+            ratings = self.item_ratings()
+            n_features = self.n_users
+        else:
+            raise ValueError
+
+        train, test = train_test_split(ratings, test_size=0.2)
+        valid, test = train_test_split(test, test_size=0.5)
+
+        return train, valid, test
+
+
+class DatasetFactory:
+    """
+    This class represents a factory that is responsible for generating datasets
+
+    """
+
     def __init__(
         self,
         ratings_path: str = None,
         items_path: str = None,
         users_path: str = None,
         sep: str = ",",
-        filter: Callable[[pd.DataFrame], pd.DataFrame] = lambda x: x,
     ):
         """
-        This class represents a way to load a surprise dataset and build the full trainset with additional filtering applied
-
         Parameters
         ----------
-        ratings_path : str, optional
+        ratings_path : str
             path to the ratings file containing information about user item interaction, by default None
         items_path : str, optional
             path to the items file containing information about items, by default None
@@ -29,14 +77,18 @@ class Dataset:
             path to the items file containing information about users, by default None
         sep : str, optional
             character or sequence of characters used to separate values, by default ","
-        filter_strategies : List[function], optional
-            list of functions to be applied to dataframe sequentialy, by default []
         """
+
+        # read data from csv files and join them into a single dataframe
+        self.df = self.read_csv(ratings_path, items_path, users_path, sep)
+
+    def read_csv(self, ratings_path, items_path, users_path, sep) -> pd.DataFrame:
         ratings_df = None
         users_df = None
         items_df = None
 
-        # load all data into their respective dataframes
+        logger = logging.getLogger("core.data")
+
         if ratings_path is not None:
             try:
                 ratings_df = pd.read_csv(
@@ -44,10 +96,9 @@ class Dataset:
                     sep=sep,
                     engine="python",
                     encoding="latin-1",
-                    low_memory=True,
                 )
             except:
-                pass
+                logger.error("Failed to load ratings file")
 
         # lets assume that the ratings_df is mandatory and the others are optional
         if items_path is not None:
@@ -60,7 +111,7 @@ class Dataset:
                     low_memory=True,
                 )
             except:
-                pass
+                logger.error("Failed to load items file")
 
         if users_path is not None:
             try:
@@ -72,48 +123,34 @@ class Dataset:
                     low_memory=True,
                 )
             except:
-                pass
+                logger.error("Failed to load users file")
 
-        self.filter = filter
+        return build_full_df(ratings_df, users_df, items_df)
 
-        self.df = build_full_df(ratings_df, users_df, items_df)
-        self.df = str_to_timestamp(self.df)
-
-    @property
-    def trainset(self) -> surprise.Trainset:
-        if hasattr(self, "_trainset"):
-            return self._trainset
-
-        self.apply_filter()
-
-        dataset = self.df.loc[:, ["user", "item", "rating"]]
-
-        dataset = surprise.Dataset.load_from_df(
-            dataset, surprise.Reader(line_format="user item rating")
-        )
-        # 4: build surprise trainset
-        self._trainset = dataset.build_full_trainset()
-
-        self.n_items = self._trainset.n_items
-        self.n_users = self._trainset.n_users
-
-        return self._trainset
-
-    def apply_filter(self):
-        self.df = self.filter(self.df)
-
-    def train_valid_test_split(
+    def create_dataset(
         self,
-        mode: Union["user", "item"] = "user",
-    ) -> Tuple[Dict, Dict, Dict]:
-        # we select what are going to be the training cases
-        if mode == "user":
-            ratings = self.trainset.ur
-        elif mode == "item":
-            ratings = self.trainset.ir
-        else:
-            raise ValueError
+        transformations: Callable[[pd.DataFrame], pd.DataFrame] = lambda x: x,
+        filters: Callable[[pd.DataFrame], pd.DataFrame] = lambda x: x,
+    ) -> Dataset:
+        """
+        Filters and transforms a copy of the underlying dataframe, and returns a Dataset object
 
-        train, test = train_test_split(ratings, test_size=0.2)
-        valid, test = train_test_split(test, test_size=0.5)
-        return train, valid, test
+        Parameters
+        ----------
+        transformations : Callable, optional
+            Transformations to be applied to the data, by default lambda*args:args
+        filters : Callable, optional
+            Filters to be applied after the transformations, by default lambda*args:args
+
+        Returns
+        -------
+        Dataset
+            Instance of a Dataset Class
+        """
+        df = filters(transformations(self.df))
+        df = df.loc[:, ["user", "item", "rating"]]
+        trainset = surprise.Dataset.load_from_df(
+            df, surprise.Reader(line_format="user item rating")
+        ).build_full_trainset()
+        dataset = Dataset(trainset)
+        return dataset
